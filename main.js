04f69356157ba822eab7a2a395bf86c28380dc2d
HiIamthis <75521620+HiIamthis@users.noreply.github.com>
2022-01-15 14:31:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+menu_list_array = ["Tandoori pizza","Veg suprme pizza","Paneer tikka Pizza","Veg Extravaganza Pizza"];
+function getMenu(){
+    var htmldata="<ol classes='menulist'>"
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+    }
+    htmldata=htmldata+"</ol>"
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+    var htmldata
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>"
+    for(var i=0;i<menu_list_array.length;i++){
+
+        htmldata='<div class="card">' +'<img src="images/pizzaImg.png"/>' + menu_list_array[i];
+    }
+    htmldata=htmldata+"</section>"
+    document.getElementById("display_addedmenu").innerHTML = htmldata;
+    
+
+}
